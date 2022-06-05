f6883f086dbf67a71ca8c3ed977a65c2fd2826d4
Pashka <51019643+Advancedboy@users.noreply.github.com>
2022-06-05 23:10:37 +0300

    Added project files

---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventt
+{
+    internal class Person
+    {
+        public event Action GoToSleep;
+        public event EventHandler DoWork;
+        public string Name { get; set; }
+
+        public void TakeTime(DateTime now)
+        {
+            if(now.Hour <= 10)
+            {
+                GoToSleep?.Invoke();
+            }
+            else
+            {
+                //var arg = new EventArgs();
+                DoWork?.Invoke(this, null);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventt
+{
+    internal class Program
+    {
+        //public delegate void ValueDelegate(int i);
+        //public delegate void MyDelegate();
+        //по сути одно и то же
+        //public event MyDelegate Event;
+        //public event Action Eventt;
+
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Pavel"
+            };
+            person.GoToSleep += Person_GoToSleep;
+            person.DoWork += Person_DoWork;
+            person.TakeTime(DateTime.Parse("6/5/2022 10:33:11pm"));
+            person.TakeTime(DateTime.Parse("6/6/2022 3:33:11am"));
+            Console.ReadLine();
+        }
+
+        private static void Person_DoWork(object sender, EventArgs e)
+        {
+            if(sender is Person)
+            Console.WriteLine($"{((Person)sender).Name} is working.");
+        }
+
+        private static void Person_GoToSleep()
+        {
+            Console.WriteLine("Man went to bed.");
+        }
+    }
+}
